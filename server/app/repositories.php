e99080ce7a11a82e03b20c4335225dc690844516
Wennlys Oliveira <wennlys@gmail.com>
2020-09-12 20:12:40 -0300

    repositories file naming update

--- server/app/repositories.php
+++ server/app/repositories.php
@@ -2,12 +2,12 @@
 declare(strict_types=1);
 
 use App\Domain\User\UserRepository;
-use App\Infrastructure\Persistence\User\InMemoryUserRepository;
+use App\Infrastructure\Persistence\User\UserRepositoryImpl;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
     // Here we map our UserRepository interface to its in memory implementation
     $containerBuilder->addDefinitions([
-        UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
+        UserRepository::class => \DI\autowire(UserRepositoryImpl::class),
     ]);
 };
