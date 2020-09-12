67d620514d1bb67649f3a60112cadabc1352a218
Wennlys Oliveira <wennlys@gmail.com>
2020-09-12 20:13:19 -0300

    database connection class created

---
+++ server/src/Infrastructure/Connection.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use PDO;
+use Exception;
+use PDOException;
+
+final class Connection
+{
+    private const OPTIONS = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+    ];
+
+    /** @var Exception|PDOException */
+    public static $error;
+
+    private static ?Connection $instance = null;
+
+    private ?PDO $conn = null;
+
+    private function __construct()
+    {
+        try {
+            $this->conn = new PDO(
+                $_ENV['HOST'] . $_ENV['NAME'],
+                $_ENV['USER'],
+                $_ENV['PASSWORD'],
+                self::OPTIONS
+            );
+        } catch (PDOException $e) {
+            self::$error = $e->getMessage();
+        }
+    }
+
+    public static function getInstance(): self
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection(): ?PDO
+    {
+        return $this->conn;
+    }
+}
