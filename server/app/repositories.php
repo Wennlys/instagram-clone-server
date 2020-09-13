fc280d89a3937e41124c046ec68ba461ce509758
Wennlys Oliveira <wennlys@gmail.com>
2020-09-13 17:39:37 -0300

    comment removal

--- server/app/repositories.php
+++ server/app/repositories.php
@@ -6,7 +6,6 @@ use App\Infrastructure\Persistence\User\UserRepositoryImpl;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
-    // Here we map our UserRepository interface to its in memory implementation
     $containerBuilder->addDefinitions([
         UserRepository::class => \DI\autowire(UserRepositoryImpl::class),
     ]);
