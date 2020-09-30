1850ae18048a7c12196c61c367d92fcadc9ea228
Wennlys Oliveira <wennlys@gmail.com>
2020-09-30 20:23:31 -0300

    initial client commit

---
+++ client/src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const rendered = render(<App />);
+  expect(rendered).toMatchSnapshot();
+});
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
     </div>
   );
 }
+
+export default App;
