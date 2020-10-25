2b55ab54e820075179407180e879134ff7fb3679
Wennlys Oliveira <wennlys@gmail.com>
2020-10-25 17:22:33 -0300

    middleware aplications

--- server/app/middleware.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use Slim\App;
-
-return function (App $app) {
-};
--- server/app/routes.php
+++ server/app/routes.php
@@ -6,6 +6,7 @@ use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\UpdateUserAction;
 use App\Application\Actions\User\ViewUserAction;
 use App\Application\Actions\Session\SessionCreateAction;
+use App\Application\Middleware\SessionMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -17,14 +18,15 @@ return function (App $app) {
         return $response;
     });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-        $group->post('', CreateUserAction::class);
-        $group->put('/{id}', UpdateUserAction::class);
-    });
-
+    $app->get('/users/{id}', ViewUserAction::class);
+    $app->post('/users', CreateUserAction::class);
+    
     $app->group('/sessions', function (Group $group) {
         $group->post('', SessionCreateAction::class);
     });
+    
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->put('/{id}', UpdateUserAction::class);
+    })->add(SessionMiddleware::class);
 };
