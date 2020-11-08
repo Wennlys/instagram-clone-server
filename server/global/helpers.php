0f44771397c625035919059dc3e3305034884b02
Wennlys Oliveira <wennlys@gmail.com>
2020-11-07 21:12:58 -0300

    relative folder helpers added

--- server/global/helpers.php
+++ server/global/helpers.php
@@ -12,3 +12,18 @@ function copyArray(array $array): array
     $arrObj = new ArrayObject($array);
     return $arrObj->getArrayCopy();
 }
+
+function deleteFileFromFolder(string $filepath): void
+{
+    array_map( 'unlink', array_filter((array) glob(getcwd() . $filepath)));
+}
+
+function TMP_DIR()
+{
+    return getcwd() . '/public/tmp/';
+}
+
+function ASSETS_DIR()
+{
+    return getcwd() . '/public/assets/';
+}
