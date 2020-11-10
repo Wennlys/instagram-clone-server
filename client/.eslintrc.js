95e69569e46f47266e3fe2f0cf11cfb6a697c935
Wennlys Oliveira <wennlys@gmail.com>
2020-11-10 16:45:49 -0300

    Eslint/Prettier config

---
+++ client/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    'jest/globals': true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['jest-dom', 'jest'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    'jest/expect-expect': 'off',
+  },
+};
---
+++ client/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+};
