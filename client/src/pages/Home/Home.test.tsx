6ab742119cc40cd9b37d85c384fad2bad0031ebf
Wennlys Oliveira <wennlys@gmail.com>
2020-11-15 01:56:02 -0300

    loadPosts implementation & tests

--- client/src/pages/Home/Home.test.tsx
+++ client/src/pages/Home/Home.test.tsx
@@ -1,15 +1,34 @@
 import { screen } from '@testing-library/dom';
-import { render } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import Home from '.';
-describe('Login rendering', () => {
-  test('login component renders correctly', () => {
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>,
-    );
+import api from '../../services/api';
+import { postsResponseMock, renderWithRouter } from '../../utils/test.utils';
+
+jest.mock('../../services/api');
+const mockedApi = api as jest.Mocked<typeof api>;
+
+afterEach(() => {
+  cleanup();
+  jest.resetAllMocks();
+});
+
+describe('Home rendering', () => {
+  test('home component renders correctly', () => {
+    mockedApi.get.mockImplementation(() => postsResponseMock.get.success);
+    renderWithRouter(<Home />);
     expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  });
+
+  it("renders 'loadPosts' component", () => {
+    mockedApi.get.mockImplementation(() => postsResponseMock.get.success);
+    renderWithRouter(<Home />);
+    expect(screen.getByText(/PostList/i)).toBeInTheDocument();
+  });
+
+  it('loads at least one post', () => {
+    mockedApi.get.mockImplementation(() => postsResponseMock.get.success);
+    renderWithRouter(<Home />);
+    expect(screen.getByTitle(/postlist/i)).toBeInTheDocument();
   });
 });
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
+import { AxiosResponse } from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Posts, { IPost } from '../../components/PostList';
+import api from '../../services/api';
 
 function Home(): JSX.Element {
+  async function loadPosts(): Promise<AxiosResponse<IPost[]>> {
+    return await api.get('/posts');
+  }
+
   return (
     <>
       <Link to="/explore">Explore</Link>
       <div>Home</div>
+      <Posts loadPosts={loadPosts} />
     </>
   );
 }
