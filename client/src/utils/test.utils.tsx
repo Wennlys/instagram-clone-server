2a41e377a47ccae07790c37c14c6db2899e4e7d1
Wennlys Oliveira <wennlys@gmail.com>
2020-11-24 20:32:51 -0300

    postResponseMock helper updated

--- client/src/utils/test.utils.tsx
+++ client/src/utils/test.utils.tsx
@@ -35,7 +35,18 @@ const postsResponseMock = {
   get: {
     success: Promise.resolve({
       data: [
-        { image: 'http://localhost:3333/tmp/avatar.jpg', description: 'Nothing to see here. :P', userName: 'user1' },
+        {
+          id: 1,
+          image: 'http://localhost:3333/tmp/avatar.jpg',
+          description: 'Nothing to see here. :P',
+          userName: 'user1',
+        },
+        {
+          id: 2,
+          image: 'http://localhost:3333/tmp/avatar.jpg',
+          description: 'Nothing to see here. :P',
+          userName: 'user2',
+        },
       ],
     }),
   },
