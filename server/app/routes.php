bb99ed68ddb5aada598d4cf88d0cd77d0abe6b5e
Wennlys Oliveira <wennlys@gmail.com>
2020-12-25 23:06:16 -0300

    Bug fix: CorsMiddleware removed because it was making tests not pass

--- server/app/routes.php
+++ server/app/routes.php
@@ -30,6 +30,15 @@ return function (App $app) {
         return $response;
     });
 
+    $app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
+    });
+
     $app->post('/users', CreateUserAction::class);
 
     $app->group('/sessions', function (Group $group) {
