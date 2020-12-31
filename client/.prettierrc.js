71794b23a9ba964d7c9a4179730981c7024db8c9
Wennlys Oliveira <wennlys@gmail.com>
2020-12-31 10:24:35 -0300

    Remaking the whole frontend following the right architecture

--- client/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: true,
-  arrowParens: 'avoid',
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-};
--- client/src/pages/Explore/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Explore: React.FC = () => {
-  return <div>Explore</div>;
-};
-
-export default Explore;
