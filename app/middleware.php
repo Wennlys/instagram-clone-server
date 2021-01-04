26c22fd9d4e8d0aa81922247e051da71cff1ecbc
Wennlys Oliveira <wennlys@gmail.com>
2021-01-04 15:47:15 -0300

    feat: created JsonBodyParserMiddleware

--- app/middleware.php
+++ app/middleware.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use App\Application\Middleware\JsonBodyParserMiddleware;
 use Slim\App;
 
 return function (App $app) {
+    $app->add(new JsonBodyParserMiddleware());
 };
