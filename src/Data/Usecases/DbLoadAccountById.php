a2479d588b7162a557bd39d4de1e68b2caeb2ec7
Wennlys Oliveira <wennlys@gmail.com>
2021-01-06 21:25:01 -0300

    feat: created DbLoadAccountById use case

--- src/Data/Usecases/DbLoadAccountById.php
+++ src/Data/Usecases/DbLoadAccountById.php
@@ -3,11 +3,19 @@ declare(strict_types=1);
 
 namespace App\Data\Usecases;
 
+use App\Data\Protocols\Db\User\FindUserOfIdRepository;
 use App\Domain\Usecases\LoadAccountById;
 
 class DbLoadAccountById implements LoadAccountById {
+    private FindUserOfIdRepository $findUserOfIdRepository;
+
+    public function __construct(FindUserOfIdRepository $findUserOfIdRepository)
+    {
+        $this->findUserOfIdRepository = $findUserOfIdRepository;
+    }
+
     public function load(int $id): array
     {
-        return [];
+        return $this->findUserOfIdRepository->findUserOfId($id);
     }
 }
