f349101ac107df68300ae815234717ed4c012452
Wennlys Oliveira <wennlys@gmail.com>
2021-01-06 21:45:24 -0300

    feat: added LoadAccountById use case to make CreateUserAction returns the created user

--- src/Application/Actions/User/CreateUserAction.php
+++ src/Application/Actions/User/CreateUserAction.php
@@ -5,16 +5,20 @@ namespace App\Application\Actions\User;
 
 use App\Domain\Models\User;
 use App\Domain\Usecases\AddUser;
+use App\Domain\Usecases\LoadAccountById;
 use App\Presentation\Errors\User\DuplicatedUserException;
+use App\Presentation\Errors\User\UserCouldNotBeCreatedException;
 use App\Presentation\Protocols\HttpResponse;
 
 class CreateUserAction
 {
     private AddUser $addUser;
+    private LoadAccountById $loadAccountById;
 
-    public function __construct(AddUser $addUser)
+    public function __construct(AddUser $addUser, LoadAccountById $loadAccountById)
     {
        $this->addUser = $addUser;
+       $this->loadAccountById = $loadAccountById;
     }
 
     /**
@@ -26,6 +30,10 @@ class CreateUserAction
         if($userId === 0)
             return new HttpResponse(403, ["error" => new DuplicatedUserException()]);
 
-        return new HttpResponse(200, []);
+        $user = $this->loadAccountById->load($userId);
+        if($user === [])
+            return new HttpResponse(403, ["error" => new UserCouldNotBeCreatedException()]);
+
+        return new HttpResponse(200, ["data" => $user]);
     }
 }
