2278683b02091fe2dff67ee13be476851f1d0b44
Wennlys Oliveira <wennlys@gmail.com>
2021-01-07 21:55:31 -0300

    test: asserts ViewUserAction returns 500 when LoadAccountByUsername throws exception

--- tests/Application/Actions/User/ViewUserActionTest.php
+++ tests/Application/Actions/User/ViewUserActionTest.php
@@ -3,85 +3,34 @@ declare(strict_types=1);
 
 namespace Tests\Application\Actions\User;
 
-use App\Application\Actions\ActionError;
-use App\Application\Actions\ActionPayload;
-use App\Application\Handlers\HttpErrorHandler;
-use App\Presentation\Errors\User\UserNotFoundException;
-use App\Infrastructure\Db\SQL\UserRepository;
-use DI\Container;
+use App\Application\Actions\User\ViewUserAction;
+use App\Domain\Usecases\LoadAccountByUsername;
 use Prophecy\PhpUnit\ProphecyTrait;
-use Slim\Middleware\ErrorMiddleware;
+use Slim\Exception\HttpInternalServerErrorException;
+use Tests\Application\Actions\Mocks\LoadAccountByUsernameSpy;
 use Tests\TestCase;
 
 class ViewUserActionTest extends TestCase
 {
     use ProphecyTrait;
 
-    public function testAction()
+    private function SUTFactory(?LoadAccountByUsername $loadAccountByUsername = null): array
     {
-        $app = $this->getAppInstance();
-
-        /** @var Container $container */
-        $container = $app->getContainer();
-
-        $user = [
-            'username' => 'bill.gates',
-            'email' => 'bill.gates@mail.com',
-            'name' => 'Bill Gates'
+        $loadAccountByUsername = $loadAccountByUsername ?: new LoadAccountByUsernameSpy();
+        $SUT = new ViewUserAction($loadAccountByUsername);
+        return [
+            "SUT" => $SUT
         ];
-
-        $userRepositoryProphecy = $this->prophesize(UserRepository::class);
-        $userRepositoryProphecy
-            ->findUserOfUsername($user['username'])
-            ->willReturn($user)
-            ->shouldBeCalledOnce();
-
-        $container->set(UserRepository::class, $userRepositoryProphecy->reveal());
-
-        $request = $this->createRequest('GET', "/{$user['username']}");
-        $response = $app->handle($request);
-
-        $payload = (string) $response->getBody();
-        $expectedPayload = new ActionPayload(200, $user);
-        $serializedPayload = json_encode($expectedPayload, JSON_PRETTY_PRINT);
-
-        $this->assertEquals($serializedPayload, $payload);
     }
 
-    public function testActionThrowsUserNotFoundException()
+    /** @test */
+    public function returns_500_when_LoadAccountByUsername_throws_exception(): void
     {
-        $app = $this->getAppInstance();
-
-        $callableResolver = $app->getCallableResolver();
-        $responseFactory = $app->getResponseFactory();
-
-        $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
-        $errorMiddleware = new ErrorMiddleware($callableResolver, $responseFactory, true, false, false);
-        $errorMiddleware->setDefaultErrorHandler($errorHandler);
-
-        $app->add($errorMiddleware);
-
-        /** @var Container $container */
-        $container = $app->getContainer();
-
-        $username = 'notfounduser';
-
-        $userRepositoryProphecy = $this->prophesize(UserRepository::class);
-        $userRepositoryProphecy
-            ->findUserOfUsername($username)
-            ->willThrow(new UserNotFoundException())
-            ->shouldBeCalledOnce();
-
-        $container->set(UserRepository::class, $userRepositoryProphecy->reveal());
-
-        $request = $this->createRequest('GET', "/{$username}");
-        $response = $app->handle($request);
-
-        $payload = (string) $response->getBody();
-        $expectedError = new ActionError(ActionError::RESOURCE_NOT_FOUND, 'User not found.');
-        $expectedPayload = new ActionPayload(404, null, $expectedError);
-        $serializedPayload = json_encode($expectedPayload, JSON_PRETTY_PRINT);
-
-        $this->assertEquals($serializedPayload, $payload);
+        $this->expectExceptionMessage('Internal server error.');
+        $loadAccountByUsername = $this->prophesize(LoadAccountByUsername::class);
+        $username = 'username';
+        $loadAccountByUsername->load($username)->willThrow(HttpInternalServerErrorException::class)->shouldBeCalledOnce();
+        ["SUT" => $SUT] = $this->SUTFactory($loadAccountByUsername->reveal());
+        $SUT->handle($username);
     }
 }
