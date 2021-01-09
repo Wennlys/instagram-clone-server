834a0aa6e879d4f479cf873972c1d7723ab18707
Wennlys Oliveira <wennlys@gmail.com>
2021-01-09 16:46:06 -0300

    refactor: added getters and setters

--- src/Presentation/Protocols/HttpResponse.php
+++ src/Presentation/Protocols/HttpResponse.php
@@ -9,7 +9,27 @@ class HttpResponse {
 
     public function __construct(int $statusCode, array $body)
     {
+        $this->setStatusCode($statusCode);
+        $this->setBody($body);
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function setStatusCode(int $statusCode): void
+    {
        $this->statusCode = $statusCode;
+    }
+
+    public function getBody(): array
+    {
+        return $this->body;
+    }
+
+    public function setBody(array $body): void
+    {
        $this->body = $body;
     }
 }
