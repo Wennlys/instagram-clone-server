810d5b04ce2b01def1096d099c9cdca5ab40deb0
Wennlys Oliveira <wennlys@gmail.com>
2021-01-09 16:10:39 -0300

    feat: created HttpRequest class

---
+++ src/Presentation/Protocols/HttpRequest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Presentation\Protocols;
+
+class HttpRequest {
+    public ?array $body;
+
+    public function __construct(array $body = null)
+    {
+        $this->setBody($body);
+    }
+
+    public function setBody(array $body): void
+    {
+        if($body !== []) $this->body = $body;
+    }
+
+    public function getBody(): ?array
+    {
+        return $this->body;
+    }
+}
