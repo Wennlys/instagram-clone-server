b80ba321196fd62adbaaf5f28b63d7b3fb1e86f2
Wennlys Oliveira <wennlys@gmail.com>
2021-01-09 18:12:41 -0300

    feat: created LoadPostById use case implementation

--- src/Data/Usecases/DbLoadPostById.php
+++ src/Data/Usecases/DbLoadPostById.php
@@ -3,11 +3,19 @@ declare(strict_types=1);
 
 namespace App\Data\Usecases;
 
+use App\Data\Protocols\Db\Post\FindPostOfIdRepository;
 use App\Domain\Usecases\LoadPostById;
 
 class DbLoadPostById implements LoadPostById {
+    private FindPostOfIdRepository $findPostOfIdRepository;
+
+    public function __construct(FindPostOfIdRepository $findPostOfIdRepository)
+    {
+       $this->findPostOfIdRepository = $findPostOfIdRepository;
+    }
+
     public function load(int $id): array
     {
-        return [];
+        return $this->findPostOfIdRepository->findPostOfId($id);
     }
 }
