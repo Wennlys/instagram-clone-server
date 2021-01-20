17ec4e352fdf9528445a407ce3c728966a087a84
Wennlys Oliveira <wennlys@gmail.com>
2021-01-20 18:47:57 -0300

    feat: action now returns authentication token when user is created

--- src/Presentation/Actions/User/CreateUserAction.php
+++ src/Presentation/Actions/User/CreateUserAction.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Presentation\Actions\User;
 
 use App\Domain\Usecases\AddUser;
+use App\Domain\Usecases\Authentication;
 use App\Domain\Usecases\LoadAccountById;
 use App\Presentation\Actions\Action;
+use App\Presentation\Errors\Http\HttpInternalServerErrorException;
 use App\Presentation\Errors\User\DuplicatedUserException;
 use App\Presentation\Errors\User\UserCouldNotBeCreatedException;
 use App\Presentation\Protocols\HttpRequest as Request;
@@ -16,11 +18,13 @@ class CreateUserAction implements Action
 {
     private AddUser $addUser;
     private LoadAccountById $loadAccountById;
+    private Authentication $authentication;
 
-    public function __construct(AddUser $addUser, LoadAccountById $loadAccountById)
+    public function __construct(AddUser $addUser, LoadAccountById $loadAccountById, Authentication $authentication)
     {
         $this->addUser = $addUser;
         $this->loadAccountById = $loadAccountById;
+        $this->authentication = $authentication;
     }
 
     public function handle(Request $request): Response
@@ -36,6 +40,19 @@ class CreateUserAction implements Action
             return new Response(403, ['error' => new UserCouldNotBeCreatedException()]);
         }
 
-        return new Response(200, ['data' => $user]);
+        $authToken = $this->authentication->authenticate($user['username'], $user['password']);
+        if ($authToken === null) {
+            return new Response(500, ['error' => new HttpInternalServerErrorException()]);
+        }
+
+        return new Response(
+            200,
+            [
+                'data' => [
+                    'user' => $user,
+                    'authToken' => $authToken,
+                ],
+            ]
+        );
     }
 }
