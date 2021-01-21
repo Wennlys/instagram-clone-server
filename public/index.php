b19350cfba3ffe2b920d550f47b607fd29059202
Wennlys Oliveira <wennlys@gmail.com>
2021-01-21 01:14:41 -0300

    chore: removed unused code

--- public/index.php
+++ public/index.php
@@ -5,15 +5,11 @@ declare(strict_types=1);
 use App\Presentation\Handlers\HttpErrorHandler;
 use App\Presentation\Handlers\ShutdownHandler;
 use App\Presentation\ResponseEmitter\ResponseEmitter;
-use DI\ContainerBuilder;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
 
 require __DIR__.'/../vendor/autoload.php';
-
-// Instantiate PHP-DI ContainerBuilder
-$containerBuilder = new ContainerBuilder();
 
 if (false) { // Should be set to true in production
     $containerBuilder->enableCompilation(__DIR__.'/../var/cache');
@@ -22,23 +18,6 @@ if (false) { // Should be set to true in production
 $dotenv = Dotenv::createImmutable(__DIR__, '/../.env');
 $dotenv->load();
 
-// Set up settings
-$settings = require __DIR__.'/../app/settings.php';
-$settings($containerBuilder);
-
-// Set up dependencies
-$dependencies = require __DIR__.'/../app/dependencies.php';
-$dependencies($containerBuilder);
-
-// Set up repositories
-$repositories = require __DIR__.'/../app/repositories.php';
-$repositories($containerBuilder);
-
-// Build PHP-DI Container instance
-$container = $containerBuilder->build();
-
-// Instantiate the app
-AppFactory::setContainer($container);
 $app = AppFactory::create();
 $callableResolver = $app->getCallableResolver();
 
@@ -49,9 +28,6 @@ $middleware($app);
 // Register routes
 $routes = require __DIR__.'/../app/routes.php';
 $routes($app);
-
-/** @var bool $displayErrorDetails */
-$displayErrorDetails = $container->get('settings')['displayErrorDetails'];
 
 // Create Request object from globals
 $serverRequestCreator = ServerRequestCreatorFactory::create();
