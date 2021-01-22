1a6bbd74aab62fdaab5f45f37bb3b42831b59d56
Wennlys Oliveira <wennlys@gmail.com>
2021-01-22 16:40:09 -0300

    feat: created UpdateAccountInformations implementation

--- src/Data/Usecases/DbUpdateAccountInformations.php
+++ src/Data/Usecases/DbUpdateAccountInformations.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Data\Usecases;
 
+use App\Data\Protocols\Db\User\UserUpdateRepository;
 use App\Domain\Models\User;
 use App\Domain\Usecases\UpdateAccountInformations;
 
 class DbUpdateAccountInformations implements UpdateAccountInformations
 {
+    private UserUpdateRepository $userUpdateRepository;
+
+    public function __construct(UserUpdateRepository $userUpdateRepository)
+    {
+        $this->userUpdateRepository = $userUpdateRepository;
+    }
+
     public function update(User $user, int $id): bool
     {
-        return true;
+        return $this->userUpdateRepository->update($user, $id);
     }
 }
