9683ee219f76c24f453cc8e42bdd395fe421468b
Wennlys Oliveira <wennlys@gmail.com>
2021-01-23 22:51:18 -0300

    feat: created LoadAccountByUsername implementation

--- src/Data/Usecases/DbLoadAccountByUsername.php
+++ src/Data/Usecases/DbLoadAccountByUsername.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace App\Data\Usecases;
 
+use App\Data\Protocols\Db\User\FindUserOfUsernameRepository;
 use App\Domain\Usecases\LoadAccountByUsername;
 
 class DbLoadAccountByUsername implements LoadAccountByUsername
 {
+    private FindUserOfUsernameRepository $findUserOfUsernameRepository;
+
+    public function __construct(FindUserOfUsernameRepository $findUserOfUsernameRepository)
+    {
+        $this->findUserOfUsernameRepository = $findUserOfUsernameRepository;
+    }
+
     public function load(string $username): array
     {
-        return [];
+        return $this->findUserOfUsernameRepository->findUserOfUsername($username);
     }
 }
